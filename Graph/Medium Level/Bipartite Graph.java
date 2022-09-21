85f4c1d122bdd42908a61b3925e6b3e0cae04b2b
Adarsh-801 <89645240+Adarsh-801@users.noreply.github.com>
2022-09-21 17:58:27 +0530

    Update Bipartite Graph.java

--- Graph/Medium Level/Bipartite Graph.java
+++ Graph/Medium Level/Bipartite Graph.java
@@ -1,5 +1,7 @@
 QUESTION LINK - https://practice.geeksforgeeks.org/problems/bipartite-graph/1
-EXPLANATION LINK - https://www.youtube.com/watch?v=ZBhZ1DXGrhA
+EXPLANATION LINK - 1. BFS APPROACH - https://www.youtube.com/watch?v=ZBhZ1DXGrhA
+                   2. DFS APPROACH - https://www.youtube.com/watch?v=uC884ske2uQ&list=PLgUwDviBIf0rGEWe64KWas0Nryn7SCRWw&index=10
+                     
 NOTE - A graph can be bipartite only when - 1. The graph is A-cyclic
                                             2. Even if the graph is cyclic the cycle has even number of vertices
   
@@ -59,6 +61,49 @@ class Solution
                 if(visited[nbr] == -1){
                     q.add( new Pair(nbr , rem.level+1));
                 }
+            }
+        }
+        
+        return true;
+    }
+}
+
+
+-------------------------------------------------------------------------DFS APPROACH-------------------------------------------------------------------------------
+  
+class Solution
+{
+    public boolean isBipartite(int V, ArrayList<ArrayList<Integer>>adj)
+    {
+        int colour[] = new int[V];
+        Arrays.fill(colour , -1);
+        
+        for(int i=0; i<V; i++){
+            if(colour[i] == -1){
+                if( !dfs(i , adj, colour) ){
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+    
+    public boolean dfs(int i, ArrayList<ArrayList<Integer>> adj , int[] colour){
+        
+        if(colour[i] == -1)     colour[i] = 1;
+        
+        for(Integer nbr : adj.get(i)){
+            
+            if(colour[nbr] == -1){
+                colour[nbr] = 1 - colour[i];
+                
+                if( !dfs(nbr , adj , colour)){
+                    return false;
+                }
+            }
+            else if(colour[nbr] == colour[i]){
+                return false;
             }
         }
         
