cca3b11694d40406337bbebba46487f6d8576a5a
Adarsh-801 <89645240+Adarsh-801@users.noreply.github.com>
2022-09-24 12:10:07 +0530

    Update Number of distinct subsequences.java

--- Strings/Hard Level/Number of distinct subsequences.java
+++ Strings/Hard Level/Number of distinct subsequences.java
@@ -1,5 +1,6 @@
 QUESTION LINK - https://practice.geeksforgeeks.org/problems/number-of-distinct-subsequences0909/1
 EXPLANATION LINK - 1. Using Recursion - https://www.youtube.com/watch?v=Sa5PmCFF_zI&list=PL-Jc9J83PIiFxaBahjslhBD1LiJAV7nKs&index=25
+                   2. Using Dymanic Programming - https://www.youtube.com/watch?v=9UEHPiK53BA
   
 ======================================================================ANSWER===========================================================================================
   
@@ -37,4 +38,35 @@ class Solution {
 
 
 --------------------------------------------------------------OPTIMIZED APPROACH------------------------------------------------------------------------------------
+  
+class Solution {
+    int distinctSubsequences(String S) {
+        
+        int n = S.length();
+        int mod = 1000000007;
+        
+        HashMap<Character , Integer> lastOccurence = new HashMap<>();
+        
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        
+        for(int i=1; i<dp.length; i++){
+            
+            dp[i] = (2*dp[i-1])%mod ;
+            
+            char ch = S.charAt(i-1);
+            
+            if(lastOccurence.containsKey(ch)){
+                int j = lastOccurence.get(ch);
+                dp[i] = (dp[i] - dp[j-1] + mod)%mod ;
+            }
+            
+            lastOccurence.put(ch , i);
+        }
+        
+        int ans = dp[n];
+        
+        return (ans)%mod;
+    }
+}
   
